063edde38846c3b7ad8e4104f81eaf4b9b35fe69
Nikolaos Kokolantonakis <eukprice@gmail.com>
2021-07-11 02:28:43 +0300

    moved activity creation date validation to separate js file

---
+++ MVCClient/wwwroot/js/ValidateActivityDates.js
@@ -0,0 +1,26 @@
+﻿
+
+$(document).ready(function () {
+
+    $("#StartDate").focusout(function () {
+        document.getElementById("submitButton").disabled = false;
+        ValidateDates();
+    });
+
+    $("#EndDate").focusout(function () {
+        document.getElementById("submitButton").disabled = false;
+        ValidateDates();
+    });
+
+})
+
+function ValidateDates() {
+
+    if (($("#StartDate").val() != "") && ($("#EndDate").val() != "")) {
+
+        if ($("#StartDate").val() > $("#EndDate").val()) {
+            document.getElementById("submitButton").disabled = true;
+            alert(`End date can't be earlier than Start date`);
+        }
+    }
+}
